fafbfece5e4a9fed7ad19ca9144a61838a61aa73
boyun <boyunk@uci.edu>
2023-07-04 09:40:55 -0700

    D17: Implement Add button

--- functions14.py
+++ functions14.py
@@ -9,12 +9,12 @@ def get_todos(filepath=FILEPATH):            # the "todos.txt" is a default argu
         todos_local = file_local.readlines()
     return todos_local
 
-print(help(get_todos))
+#print(help(get_todos))
 def write_todos(todos_arg, filepath=FILEPATH):              # parameters have been added # non-default parameters should come first
     """Write the to-do items list in the text file"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
-    return todos                #function return
+                #function return
 
 # print("hello from functions")
 # note that importing in TodoList program executes the print
@@ -22,7 +22,7 @@ def write_todos(todos_arg, filepath=FILEPATH):              # parameters have be
 # this will produce an error, because x is not defined
 # it will also immediately terminate the program
 
-print(__name__)                     # executes only when the functions itself is executed
+#print(__name__)                     # executes only when the functions itself is executed
 
 if __name__ == "__main__":          # if conditional block
     print("Hello")
