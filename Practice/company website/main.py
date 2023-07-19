90991012f61ca87447d337b2f52d9ca9d2341a27
boyun <boyunk@uci.edu>
2023-07-19 10:25:17 -0700

    D22: Edits based on solutions

--- Practice/company website/main.py
+++ Practice/company website/main.py
@@ -15,18 +15,18 @@ df =  pandas.read_csv("data.csv")
 
 with col1:
     for index, row in df[:4].iterrows():
-        st.header(row["first name"] + " " +row["last name"])
+        st.header(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
         st.image("images/"+row["image"])
 
 with col2:
     for index, row in df[4:8].iterrows():
-        st.header(row["first name"] + " " +row["last name"])
+        st.header(row["first name"].title() + " " +row["last name"].title())
         st.write(row["role"])
         st.image("images/"+row["image"])
 
 with col3:
     for index, row in df[8:12].iterrows():
-        st.header(row["first name"] + " " +row["last name"])
+        st.header(row["first name"].title() + " " +row["last name"].title())
         st.write(row["role"])
         st.image("images/"+row["image"])
