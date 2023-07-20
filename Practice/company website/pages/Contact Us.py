ee91a04564c3ff1a08be0172fd606db22d2a8376
boyun <boyunk@uci.edu>
2023-07-20 13:45:50 -0700

    D23: Edits from solution (doesn't work)

--- Practice/company website/pages/Contact Us.py
+++ Practice/company website/pages/Contact Us.py
@@ -1,21 +1,24 @@
 import streamlit as st
 from email import send_email
+import pandas
+
+df = pandas.read_csv("topics.csv")
 
 st.header("Contact Us")
 
-with st.form(key="email_forms"):
+with st.form(key="email_form"):
     user_email = st.text_input("your email address")
     option = st.selectbox('What topic do you want to discuss?',
-                           ("Job Inquiries", "Project Proposals","Other"))
-    raw_message = st.text_area("Your message")
+                          df["topic"])
+    raw_message = st.text_area("Text")
 
     message = f"""\
-    Subject: New email from company profile about {option}
-    From: {user_email}
-    {raw_message}
-    """
+Subject: New email from company profile about {option}
+
+From: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
-    print(button)
     if button:
         send_email(message)
         st.info("Your email was sent successfully.")
