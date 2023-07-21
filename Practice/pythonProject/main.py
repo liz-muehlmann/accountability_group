d13b05591c3b5854770928e788e5d50e79ba0c62
boyun <boyunk@uci.edu>
2023-07-21 14:12:35 -0700

    D24: Add footers

--- Practice/pythonProject/main.py
+++ Practice/pythonProject/main.py
@@ -7,27 +7,45 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 #The topics become headers for each page
 
 for index, row in df.iterrows():
-    pdf.add_page()
+    pdf.add_page()              # the parent page
+
+    # set the header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)     #RGB combination for grew
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
          ln=1)
     pdf.line(10,22,200,22)
 
-# next I want to create the number of pages that the csv file shows
-# use for loop
-# x=3
-# for i in range(x):
-# print("Hello")
+    # set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    # next I want to create the number of pages that the csv file shows
+    # use for loop
+    # x=3
+    # for i in range(x):
+    # print("Hello")
     for i in range(row["Pages"] - 1):
         pdf.add_page()
     # this is called the nested loop
+
+    #set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0, h=10, txt = row["Topic"], align = "R")
+
+
+
 
 
 pdf.output("output.pdf")
