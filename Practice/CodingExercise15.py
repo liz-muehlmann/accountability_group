140ab85818384a1bd3418a2571f80882bd26e130
boyunk <63751743+boyunk@users.noreply.github.com>
2023-08-21 13:17:49 -0700

    Delete Practice directory

--- Practice/CodingExercise15.py
+++
@@ -1,7 +0,0 @@
-# a program that generates a random whole number
-import random
-
-low = int(input("Enter the lower bound number: "))
-high = int(input("Enter the upper bound number: "))
-
-print(random.randrange(low,high))
